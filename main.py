a05604b23d7028f03e3e5a107afffc33282d16b9
danamboss <100099797+danamboss@users.noreply.github.com>
2022-03-15 15:50:32 +0000

    added more lines

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import csv
 
-with open('data.csv') as csv file:
+with open('data.csv') as csv_file:
+  csv_reader = csv.reader(csv_file, delimiter=',')
+  line_count = 0
+
+  data = {}
+  for row in csv_reader:
+    if line_count == 0:
+      print(f'column names are {", ".join(row)}')
+      line_count +=1
+    else:
+      print(row[11])
+      line_count += 1
+  print(f'Process {line_count} lines.')    
